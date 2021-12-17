4e402166ce2db736127598ac1c142ddafb655a9b
Güldeniz Özdemir <78044804+Deniz000@users.noreply.github.com>
2021-12-17 17:58:46 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+const euroun = 11.2
+
+
+let konutKredileri = ["X Kredisi", "Y Kredisi", "Z Kredisi"]
+
+console.log("<ul>")
+for(let i = 0; i < 3;i++){
+    console.log("<li>" + konutKredileri[i]+"</li>")
+}
+console.log("<ul>")
+console.log(konutKredileri)
